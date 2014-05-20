07aa3c9288446c77ca65c819a8f9cabe0ccd5ff4
Benedikt Lang <github@benediktlang.de>
2014-05-20 09:03:40 +0200

    Concurrency fix

--- bot.go
+++ bot.go
@@ -60,6 +60,7 @@ func main() {
 	for {
 		fmt.Printf("Go on\n")
 		wg = sync.WaitGroup{}
+		wg.Add(1)
 		go processTopics()
 		wg.Wait()
 		// break
@@ -81,6 +82,7 @@ func bootstrapParsers() {
 }
 
 func processTopics() {
+	defer wg.Done()
 	feed, err := api.CategoryFeed(cfg.Category)
 	if err != nil {
 		log.Printf("Can't fetch category feed: %s", err)
